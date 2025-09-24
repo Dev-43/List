d10a64dcb0b8a2092eb111246ddfebd7e59ae4e5
Dev-43 <deveshdolas9@gmail.com>
2025-09-24 19:22:03 +0530

    Changes

--- script.js
+++ script.js
@@ -10,10 +10,26 @@ searchButton.addEventListener("click", function() {
     
     const resultCard=document.createElement('div');
     resultCard.classList.add('result-card');
-    resultCard.textContent=`
-    <p class="result-title">${query}</p>;
+    resultCard.innerHTML=`
+    <p class="result-title">${query}</p>
     <button class="add-btn">Add to List</button>`;
 
     resultsContainer.appendChild(resultCard);
-
 });
+
+resultsContainer.addEventListener("click",function(event){
+
+    if(event.target.classList.contains("add-btn")){
+        console.log("Add Button Clicked!");
+    }
+
+    const clickedButton=event.target;
+    clickedButton.textContent='Added!';
+    clickedButton.disabled=true;
+
+    clickedButton.classList.remove("add-btn");
+    clickedButton.classList.add("added-btn");
+
+    console.log("Item Added!");
+}
+)
